9a0ca37a6b61e454de8038fad07b0a00a429d7fb
shirlyfang <shirly.is.here@gmail.com>
2018-11-04 15:06:38 -0800

    added tab navigator

--- App.js
+++ App.js
@@ -7,12 +7,26 @@ import ViewContainer from './app/components/ViewContainer'
 import MainFeed from './app/screens/MainFeed'
 import Tests from './app/screens/Tests'
 import Notes from './app/screens/Notes'
+import Locations from './app/screens/Locations'
+import Profile from './app/screens/Profile'
 import { StackNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation';
 
-const AppNavigator = StackNavigator({
+const MainStack = StackNavigator({
   MainFeed: { screen: MainFeed },
   Tests: {screen: Tests},
   Notes: {screen: Notes},
+});
+const AppTabNavigator = createBottomTabNavigator({
+  MainStack: { screen: MainFeed },
+  Locations: { screen: Locations },
+  Profile: { screen: Profile },
+},
+{
+    initialRouteName: 'MainStack',
+    activeColor: '#f0edf6',
+    inactiveColor: '#3e2465',
+    barStyle: { backgroundColor: '#694fad' },
 });
 
 
@@ -23,9 +37,11 @@ export default class App extends Component<Props> {
    
   }
   render() {
-    return (
-          <AppNavigator style = {styles.navigator}
+    return ( 
+      <AppTabNavigator>  
+        <MainStack style = {styles.navigator}
             />
+            </AppTabNavigator>
     )
   }
 }
---
+++ app/screens/Profile.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, FlatList, Button, TouchableOpacity} from 'react-native';
+import StatusBarBackground from '../components/StatusBarBackground'
+import ViewContainer from '../components/ViewContainer'
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props)
+   
+  }
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <ViewContainer>
+        <Text> HI </Text>
+      </ViewContainer>
+    )
+  }
+
+}
+
+module.exports = Profile
