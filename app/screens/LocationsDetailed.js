5884a6cc9904f877ec72cd4722bcd9017b8e1aba
Matthew Wang <matthew.wang.c@gmail.com>
2019-02-02 14:26:43 -0800

    updated location list UI

--- app/screens/LocationsDetailed.js
+++ app/screens/LocationsDetailed.js
@@ -67,94 +67,110 @@ export default class LocationsDetailed extends Component {
     return (
       <ScrollView style={styles.container}>
         <Image
-          style={{
-            width, height: width, resizeMode: 'contain', marginBottom: 15
-          }}
+          style={{width:width, height:width,}}
           source={{ uri: item.Image_URL }}
         />
         <View style={styles.information}>
           <Text style={styles.Name}>{item.Name}</Text>
 
           {/* NEED TO CHANGE TO A PROGRESS BAR  */}
-          <Text style={styles.Section_Header}>
+          <Text style={styles.Activity_Level_TEMPORARY}>
 Activity Level:
             {' '}
             {item.Activity_Level}
 %
           </Text>
-
-          <Text style={styles.Section_Header}>Hours:</Text>
-          <Text style={DayOfTheWeek == 'Monday' ? styles.currentDay : styles.normalDay}>
-Monday:
-            {' '}
-            {this._generateHourString(item.MondayOpen, item.MondayClosed)}
-          </Text>
-          <Text style={DayOfTheWeek == 'Tuesday' ? styles.currentDay : styles.normalDay}>
-Tuesday:
-            {' '}
-            {this._generateHourString(item.TuesdayOpen, item.TuesdayClosed)}
-          </Text>
-          <Text style={DayOfTheWeek == 'Wednesday' ? styles.currentDay : styles.normalDay}>
-Wednesday:
-            {' '}
-            {this._generateHourString(item.WednesdayOpen, item.WednesdayClosed)}
-          </Text>
-          <Text style={DayOfTheWeek == 'Thursday' ? styles.currentDay : styles.normalDay}>
-Thursday:
-            {' '}
-            {this._generateHourString(item.ThursdayOpen, item.ThursdayClosed)}
-          </Text>
-          <Text style={DayOfTheWeek == 'Friday' ? styles.currentDay : styles.normalDay}>
-Friday:
-            {' '}
-            {this._generateHourString(item.FridayOpen, item.FridayClosed)}
-          </Text>
-          <Text style={DayOfTheWeek == 'Saturday' ? styles.currentDay : styles.normalDay}>
-Saturday:
-            {' '}
-            {this._generateHourString(item.SaturdayOpen, item.SaturdayClosed)}
-          </Text>
-          <Text style={DayOfTheWeek == 'Sunday' ? styles.currentDay : styles.normalDay}>
-Sunday:
-            {' '}
-            {this._generateHourString(item.SundayOpen, item.SundayClosed)}
-          </Text>
+          <View style = {styles.hours}>
+            <Text style={styles.Section_Header}>Hours:</Text>
+            <Text style={DayOfTheWeek == 'Monday' ? styles.currentDay : styles.normalDay}>
+  Monday:
+              {' '}
+              {this._generateHourString(item.MondayOpen, item.MondayClosed)}
+            </Text>
+            <Text style={DayOfTheWeek == 'Tuesday' ? styles.currentDay : styles.normalDay}>
+  Tuesday:
+              {' '}
+              {this._generateHourString(item.TuesdayOpen, item.TuesdayClosed)}
+            </Text>
+            <Text style={DayOfTheWeek == 'Wednesday' ? styles.currentDay : styles.normalDay}>
+  Wednesday:
+              {' '}
+              {this._generateHourString(item.WednesdayOpen, item.WednesdayClosed)}
+            </Text>
+            <Text style={DayOfTheWeek == 'Thursday' ? styles.currentDay : styles.normalDay}>
+  Thursday:
+              {' '}
+              {this._generateHourString(item.ThursdayOpen, item.ThursdayClosed)}
+            </Text>
+            <Text style={DayOfTheWeek == 'Friday' ? styles.currentDay : styles.normalDay}>
+  Friday:
+              {' '}
+              {this._generateHourString(item.FridayOpen, item.FridayClosed)}
+            </Text>
+            <Text style={DayOfTheWeek == 'Saturday' ? styles.currentDay : styles.normalDay}>
+  Saturday:
+              {' '}
+              {this._generateHourString(item.SaturdayOpen, item.SaturdayClosed)}
+            </Text>
+            <Text style={DayOfTheWeek == 'Sunday' ? styles.currentDay : styles.normalDay}>
+  Sunday:
+              {' '}
+              {this._generateHourString(item.SundayOpen, item.SundayClosed)}
+            </Text>
+          </View>
         </View>
       </ScrollView>
     );
   }
 }
 
+/* Standardized text used throughout code */
+const text = {
+  fontFamily: 'System',
+  letterSpacing: 1.92,
+}
+
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#e0e0e0',
+    backgroundColor: 'white',
+    flexDirection:'column',
   },
   information: {
-    marginLeft: 10,
-    marginRight: 10,
+    padding:20,
+    flexDirection: 'column',
+    justifyContent:'flex-end',
   },
   Name: {
-    fontSize: 30,
+    ...text,
+    fontWeight: 'bold',
+    fontSize: 25,
     color: '#000',
     textAlign: 'left',
+    flex:1,
+  },
+  Activity_Level_TEMPORARY: {
+    color:'#5e5b59',
+    textAlign: 'left',
+    paddingTop: 10,
+    paddingBottom:30,
+    fontSize: 15,
+    flex:1,
+  },
+  hours:{
+    flex:1,
   },
   Section_Header: {
+    ...text,
+    fontWeight:'bold',
     fontSize: 20,
     color: '#000',
     textAlign: 'left',
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  Activity_Level_TEMPORARY: {
-    textAlign: 'center',
-    fontSize: 15,
-    fontStyle: 'italic',
+    paddingBottom: 5,
   },
   currentDay: {
     fontWeight: 'bold',
   },
   normalDay: {
-
   }
 });
 
