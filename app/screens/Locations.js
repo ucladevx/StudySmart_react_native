e76921c9cade29cd65c24bdcdb5ddf3664462383
Benjamin Chen He <benhe@s-164-67-209-102.resnet.ucla.edu>
2019-03-13 03:44:48 -0700

    changed location titles to match menu

--- app/screens/Locations.js
+++ app/screens/Locations.js
@@ -21,16 +21,16 @@ export default class Locations extends Component {
     
     componentDidMount(){
         const markerList = [
-                    {title: "Powell", latlng: {latitude: 34.071613, longitude: -118.442181}},
-                    {title: "Arts", latlng: {latitude: 34.074456, longitude: -118.439205}},
-                    {title: "Rosenfeld Management", latlng: {latitude: 34.074281, longitude: -118.443350}},
-                    {title: "Southern Regional", latlng: {latitude: 34.071090, longitude: -118.454179}},
-                    {title: "Rudolph East Asian", latlng: {latitude:  34.074960, longitude: -118.441466}},
-                    {title: "Science & Engineering", latlng: {latitude: 34.068986, longitude: -118.442659}},
-                    {title: "Music", latlng: {latitude: 34.070693, longitude: -118.440154}},
-                    {title: "Darling Law", latlng: {latitude: 34.072646, longitude: -118.437929}},
-                    {title: "Young Research", latlng: {latitude: 34.074970, longitude: -118.441464}},
-                    {title: "Darling Biomedical", latlng: {latitude: 34.066654, longitude: -118.442417}}
+                    {title: "Powell Library", latlng: {latitude: 34.071613, longitude: -118.442181}},
+                    {title: "Arts Library", latlng: {latitude: 34.074456, longitude: -118.439205}},
+                    {title: "Management Library (Eugene and Maxine Rosenfeld)", latlng: {latitude: 34.074281, longitude: -118.443350}},
+                    {title: "Southern Regional Library Facility", latlng: {latitude: 34.071090, longitude: -118.454179}},
+                    {title: "East Asian Library (Richard C. Rudolph)", latlng: {latitude:  34.074960, longitude: -118.441466}},
+                    {title: "Science and Engineering Library", latlng: {latitude: 34.068986, longitude: -118.442659}},
+                    {title: "Music Library", latlng: {latitude: 34.070693, longitude: -118.440154}},
+                    {title: "Law Library (Hugh & Hazel Darling)", latlng: {latitude: 34.072646, longitude: -118.437929}},
+                    {title: "Research Library (Charles E. Young)", latlng: {latitude: 34.074970, longitude: -118.441464}},
+                    {title: "Biomedical Library (Louise M. Darling)", latlng: {latitude: 34.066654, longitude: -118.442417}}
                           ];
         this.setState({ markers: markerList })
     }
