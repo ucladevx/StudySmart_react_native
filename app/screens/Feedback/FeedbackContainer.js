1fa6f38a7c22fb987a10fd87bfd4fa811e0ee4e7
shirlyfang <shirly.is.here@gmail.com>
2019-05-28 23:22:37 -0700

    changed feedback link

--- app/screens/Feedback/FeedbackContainer.js
+++ app/screens/Feedback/FeedbackContainer.js
@@ -6,7 +6,7 @@ const FeedbackContainer = createMaterialTopTabNavigator(
   {
     Bugs: { screen: () => <Feedback link="https://forms.gle/Wc2v4nQyqxj89oUq7" /> },
     Feedback: { screen: () => <Feedback link="https://forms.gle/khhjTShNv54Yif3B9" /> },
-    Privacy: { screen: () => <Feedback link="https://app.termly.io/document/privacy-policy/022aff84-e92c-4f17-bcdc-fb43211733ba" /> },
+    Privacy: { screen: () => <Feedback link="https://drive.google.com/file/d/13KceghuhhVlXsHjaml55t5SEc9wN6zdd/view?usp=sharing" /> },
   },
   {
     swipeEnabled: true,
